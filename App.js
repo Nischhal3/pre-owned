fa5916b6995c9468fbcd720df41f17fd83e1042f
anniehuynh <anniehuynh253@gmail.com>
2022-02-01 15:10:58 +0200

    splash screen responsive tested, ready

--- App.js
+++ App.js
@@ -1,23 +1,20 @@
 import React from 'react';
 import {StatusBar} from 'expo-status-bar';
-import {StyleSheet} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
+import {StyleSheet, Text, View} from 'react-native';
 
-import Navigator from './navigation/navigator';
-import colors from './utils/colors';
 export default function App() {
   return (
-    <NavigationContainer>
-      <Navigator style={styles.container} />
+    <View style={styles.container}>
+      <Text>PreOwned</Text>
       <StatusBar style="auto" />
-    </NavigationContainer>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: colors.primary,
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
