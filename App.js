261b42d94fca9b90c63668281e750840f90f5bff
nischhas <nischhal.shrestha@metropolia.fi>
2022-02-01 20:57:08 +0200

    reverted to initial commit

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import {StatusBar} from 'expo-status-bar';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
-// Import Context
-import {MainProvider} from './contexts/MainContext';
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
 
 // Import Views
 import WelcomeScreen from './views/WelcomeScreen';
