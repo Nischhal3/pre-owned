7c45572e5a71d9df4694f4c17722abbaff5bb585
anniehuynh <anniehuynh253@gmail.com>
2022-02-01 18:26:05 +0200

    merged welcomescreen

--- App.js
+++ App.js
@@ -10,15 +10,11 @@ import WelcomeScreen from './views/WelcomeScreen';
 // Import from UI Kitten Library
 import {ApplicationProvider, Layout, Text} from '@ui-kitten/components';
 import * as eva from '@eva-design/eva';
-import {default as theme} from './utils/theme.json';
 
-// link fonts with the native side
-import {default as mapping} from './utils/mapping.json';
-
-const HomeScreen = () => <WelcomeScreen />;
+const App = () => <WelcomeScreen />;
 
 export default () => (
   <ApplicationProvider {...eva} theme={eva.light}>
-    <HomeScreen />
+    <App />
   </ApplicationProvider>
 );
