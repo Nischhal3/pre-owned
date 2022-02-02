6e9627c1edccf1db45e3a4a7a1be6ad3662e7498
Boris Hoi <Boris.Hoi@metropolia.fi>
2022-02-03 01:45:17 +0200

    Fixed keyboardavoidingview

--- views/Login.js
+++ views/Login.js
@@ -1,62 +1,66 @@
 import React, {useContext, useEffect, useState} from 'react';
-import {Image, StyleSheet, TouchableWithoutFeedback, KeyboardAvoidingView, Keyboard, Platform} from 'react-native';
+import {Image, StyleSheet, TouchableOpacity, KeyboardAvoidingView, Keyboard, Platform} from 'react-native';
 import {Card, Layout, ButtonGroup, Button, Text} from '@ui-kitten/components';
 import {primary, btnBackground, text_dark} from '../utils/colors'
 import SignupForm from '../components/SignupForm';
 import LoginForm from '../components/LoginForm';
-import {SafeAreaView} from 'react-native-safe-area-context';
 
 const Login = () => {
   const [formToggle, setFormToggle] = useState(true);
 
   return (
-    <KeyboardAvoidingView
-      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    <TouchableOpacity
       style={{flex: 1}}
+      activeOpacity={1}
+      onPress={() => Keyboard.dismiss()}
     >
-        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-          <Layout style={styles.view}>
-            <Image
-              style={styles.background}
-              source={require('../assets/backgrounds/LoginBG.png')}
-            />
-            <Card style={styles.container}>
-                <ButtonGroup
-                  style={styles.toggleGroup}
-                  selectedIndex={formToggle ? 0 : 1}>
-                  <Button style={formToggle ? styles.toggle2 : styles.toggle1} onPress={() => setFormToggle(true)}>Log In</Button>
-                  <Button style={formToggle ? styles.toggle1 : styles.toggle2} onPress={() => setFormToggle(false)}>Sign Up</Button>
-                </ButtonGroup>
-                {formToggle ? (
-                  <Card style={styles.card}>
-                    <Text category="h4" style={styles.header}>Log In</Text>
-                    <LoginForm setFormToggle={setFormToggle} />
-                  </Card>
-                ) : (
-                  <Card style={styles.card}>
-                    <Text category="h4" style={styles.header}>Sign Up</Text>
-                    <SignupForm setFormToggle={setFormToggle} />
-                  </Card>
-                )}
-            </Card>
-          </Layout>
-        </TouchableWithoutFeedback>
-    </KeyboardAvoidingView>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : ''}
+        style={styles.container}
+      >
+        <Layout style={styles.layout}>
+          <Image
+            style={styles.backgroundImg}
+            source={require('../assets/backgrounds/LoginBG.png')}
+          />
+          <Card style={styles.cardContainer}>
+              <ButtonGroup
+                style={styles.toggleGroup}
+                selectedIndex={formToggle ? 0 : 1}>
+                <Button style={formToggle ? styles.toggle2 : styles.toggle1} onPress={() => setFormToggle(true)}>Log In</Button>
+                <Button style={formToggle ? styles.toggle1 : styles.toggle2} onPress={() => setFormToggle(false)}>Sign Up</Button>
+              </ButtonGroup>
+              {formToggle ? (
+                <Card style={styles.card}>
+                  <Text category="h4" style={styles.header}>Log In</Text>
+                  <LoginForm setFormToggle={setFormToggle} />
+                </Card>
+              ) : (
+                <Card style={styles.card}>
+                  <Text category="h4" style={styles.header}>Sign Up</Text>
+                  <SignupForm setFormToggle={setFormToggle} />
+                </Card>
+              )}
+          </Card>
+        </Layout>
+      </KeyboardAvoidingView>
+    </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
-  view: {
-
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
   },
-  background: {
+  backgroundImg: {
     position: 'absolute',
     top: 0,
   },
-  container: {
+  cardContainer: {
+    top: '40%',
     width: '100%',
     height: '100%',
-    marginTop: '80%',
     backgroundColor: primary,
     borderColor: primary,
   },
@@ -72,7 +76,6 @@ const styles = StyleSheet.create({
     width: 100,
     backgroundColor: btnBackground,
     borderColor: btnBackground,
-    borderRadius: 70,
   },
   toggle2: {
     width: 100,
