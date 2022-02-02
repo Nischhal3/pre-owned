090e47ee6263e4c9211ae5efb7711d7739f5f4f5
jasminpartanen <jasmin.partanen@outlook.com>
2022-02-02 19:38:05 +0200

    started landing page

--- App.js
+++ App.js
@@ -4,15 +4,22 @@ import {StatusBar} from 'expo-status-bar';
 import {MainProvider} from './contexts/MainContext';
 // Import Views
 import WelcomeScreen from './views/WelcomeScreen';
+import LandingScreen from './views/Landing';
 
 // Import from UI Kitten Library
-import {ApplicationProvider, Layout, Text} from '@ui-kitten/components';
+import {ApplicationProvider, IconRegistry, Layout, Text} from '@ui-kitten/components';
 import * as eva from '@eva-design/eva';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
 
-const App = () => <WelcomeScreen />;
+// const App = () => <WelcomeScreen />
+const App = () => <LandingScreen />
 
 export default () => (
-  <ApplicationProvider {...eva} theme={eva.light}>
+  <>
+   <IconRegistry icons={EvaIconsPack} />
+   <ApplicationProvider {...eva} theme={eva.light}>
     <App />
   </ApplicationProvider>
+  </>
+
 );
---
+++ components/ProductList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ListItem from './ListItem';
+
+
+
+const ProductList = () => {
+  return (
+  <ListItem />
+  );
+};
+
+
+
+export default ProductList;
