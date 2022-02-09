5bf768a1425906bf8bd97d19cd0ae7fec7f6fb5d
anniehuynh <anniehuynh253@gmail.com>
2022-02-09 19:35:55 +0200

    add AppButton style to GlobalStyle

--- utils/GlobalStyles.js
+++ utils/GlobalStyles.js
@@ -1,9 +1,38 @@
-import { StyleSheet, Platform, StatusBar } from "react-native";
+import {StyleSheet, Platform, StatusBar} from 'react-native';
+import colors from './colors';
 
 export default StyleSheet.create({
   AndroidSafeArea: {
     flex: 1,
-    backgroundColor: "white",
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
-  }
+    backgroundColor: 'white',
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+  },
+  
+  btnStyleSmall: {
+    width: '50%',
+    height: 50,
+    bottom: 50,
+    margin: 10,
+    borderRadius: 19,
+    backgroundColor: colors.btnBackground,
+    borderColor: colors.btnBackground,
+  },
+
+  btnStyleMedium: {
+    width: '70%',
+    height: 60,
+    bottom: 50,
+    margin: 10,
+    borderRadius: 19,
+    backgroundColor: colors.btnBackground,
+    borderColor: colors.btnBackground,
+  },
+  btnStyleLarge: {
+    width: 300,
+    height: 60,
+    margin: 10,
+    borderRadius: 19,
+    backgroundColor: colors.btnBackground,
+    borderColor: colors.btnBackground,
+  },
 });
