9428a4873a6c7a38166d9eeb94e0909b10f4dcf6
Boris Hoi <Boris.Hoi@metropolia.fi>
2022-02-10 15:58:54 +0200

    renewing views folder

---
+++ views/MyListing.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const MyListing = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default MyListing;
