0075783213bbe9fc0ce8140e1ca5529b691f6035
anniehuynh <anniehuynh253@gmail.com>
2022-02-12 13:26:56 +0200

    add AppIcon

--- components/elements/Icons.js
+++ components/elements/Icons.js
@@ -1,11 +1,31 @@
 import {Icon} from '@ui-kitten/components';
 import React from 'react';
+import {View} from 'react-native';
 import colors from '../../utils/colors';
-
 // Right arrow for lists
 const PointRightArrow = (props) => (
   <Icon {...props} name="chevron-right-outline" />
 );
-const TrashIcon = (props) => <Icon {...props} name="trash-outline" />;
+const AppIcon = ({
+  name,
+  size = 40,
+  backgroundColor = '#fff',
+  iconColor = colors.danger,
+}) => {
+  return (
+    <View
+      style={{
+        width: size,
+        height: size,
+        borderRadius: size / 2,
+        backgroundColor,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Icon name={name} color={iconColor} size={size * 0.5} />
+    </View>
+  );
+};
 
-export {PointRightArrow, TrashIcon};
+export {PointRightArrow, AppIcon};
