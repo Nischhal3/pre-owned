3b09a8aaf08535c445c9e324402224994db27dc2
nischhas <nischhal.shrestha@metropolia.fi>
2022-02-15 16:11:36 +0200

    added component to display error message on form

--- components/SignupForm.js
+++ components/SignupForm.js
@@ -15,7 +15,7 @@ import {PropTypes} from 'prop-types';
 import FormInput from './formComponents/FormInput';
 import {FormButton} from './elements/AppButton';
 import colors from '../utils/colors';
-import ErrorMessage from './formComponents/ErrorMessage';
+import ErrorMessage from './elements/ErrorMessage';
 
 const SignupForm = ({setFormToggle}) => {
   //for checkbox
@@ -93,22 +93,18 @@ const SignupForm = ({setFormToggle}) => {
         name="username"
       />
 
-      {/* {errors.username && (
-        <Text status="danger">
-          {errors.username && errors.username.message}{' '}
-        </Text>
-      )} */}
       <ErrorMessage
         error={errors?.username}
         message={errors?.username?.message}
       />
+
       <Controller
         control={control}
         rules={{
           required: {value: true, message: 'This is required.'},
           pattern: {
             value: /\S+@\S+\.\S+$/,
-            message: 'Has to be valid email.',
+            message: 'Not valid email.',
           },
         }}
         render={({field: {onChange, onBlur, value}}) => (
@@ -127,9 +123,7 @@ const SignupForm = ({setFormToggle}) => {
         name="email"
       />
 
-      {errors.email && (
-        <Text status="danger">{errors.email && errors.email.message} </Text>
-      )}
+      <ErrorMessage error={errors?.email} message={errors?.email?.message} />
 
       <Controller
         control={control}
@@ -159,11 +153,10 @@ const SignupForm = ({setFormToggle}) => {
         name="password"
       />
 
-      {errors.password && (
-        <Text status="danger">
-          {errors.password && errors.password.message}{' '}
-        </Text>
-      )}
+      <ErrorMessage
+        error={errors?.password}
+        message={errors?.password?.message}
+      />
 
       <Controller
         control={control}
@@ -192,11 +185,10 @@ const SignupForm = ({setFormToggle}) => {
         name="confirmPassword"
       />
 
-      {errors.confirmPassword && (
-        <Text status="danger">
-          {errors.confirmPassword && errors.confirmPassword.message}{' '}
-        </Text>
-      )}
+      <ErrorMessage
+        error={errors?.confirmPassword}
+        message={errors?.confirmPassword?.message}
+      />
 
       {/* <Input style={styles.input} accessoryLeft={<Icon name="person-outline"/>} placeholder="Username" /> */}
       {/* <Input style={styles.input} accessoryLeft={<Icon name="email-outline"/>} placeholder='Email' /> */}
