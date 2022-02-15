66987fde86f64b21e24713e4c623a32cfb62fa26
anniehuynh <anniehuynh253@gmail.com>
2022-02-15 09:31:42 +0200

    animation welcomescreen is ready and styled

--- views/WelcomeScreen.js
+++ views/WelcomeScreen.js
@@ -1,5 +1,5 @@
 import React, {useContext, useEffect} from 'react';
-import {ImageBackground, StyleSheet} from 'react-native';
+import {ImageBackground, Platform, StyleSheet} from 'react-native';
 import PropTypes from 'prop-types';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {MainContext} from '../contexts/MainContext';
@@ -34,40 +34,34 @@ const WelcomeScreen = ({navigation}) => {
 
   useEffect(() => {
     checkToken();
-    // animation.current?.play();
+    animation.current?.play();
   }, []);
 
   return (
-    <ImageBackground
-      style={styles.background}
-      source={require('../assets/backgrounds/OnBoarding.png')}
-    >
-      {/* <Layout style={styles.inner}> */}
-
-      {/* <LottieView
+    <Layout style={styles.inner}>
+      <LottieView
         ref={animation}
-        source={require('../assets/brand/PreOwned-animation.json')}
-        style={styles.animation}
-        loop={true}
-      /> */}
-
-      <AppButton
-        title="Login"
-        onPress={() => {
-          setFormToggle(true);
-          navigation.navigate('Login');
-        }}
+        source={require('../assets/brand/animation.json')}
+        loop={false}
       />
-      <AppButton
-        style={{marginBottom: 50}}
-        title="Create an account"
-        onPress={() => {
-          setFormToggle(false);
-          navigation.navigate('Login');
-        }}
-      />
-      {/* </Layout> */}
-    </ImageBackground>
+      <Layout style={styles.btnContainter}>
+        <AppButton
+          title="Login"
+          onPress={() => {
+            setFormToggle(true);
+            navigation.navigate('Login');
+          }}
+        />
+        <AppButton
+          style={{marginBottom: 50}}
+          title="Create an account"
+          onPress={() => {
+            setFormToggle(false);
+            navigation.navigate('Login');
+          }}
+        />
+      </Layout>
+    </Layout>
   );
 };
 
@@ -77,18 +71,17 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-end',
     alignItems: 'center',
   },
-  // animation: {
-  //   width: '100%',
-  //   height: '70%',
-  //   marginBottom: 20,
-  //   alignSelf: 'center',
-  // },
-  // inner: {
-  //   width: 400,
-  //   height: undefined,
-  //   backgroundColor: colors.primary,
-  //   alignSelf: 'center',
-  // },
+  btnContainter: {
+    backgroundColor: 'transparent',
+    alignSelf: 'center',
+  },
+  inner: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: colors.primary,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
 });
 
 WelcomeScreen.propTypes = {
