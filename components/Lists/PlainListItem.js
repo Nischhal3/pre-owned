fd2b93b32e68009a8b81cc0512a363c78151d046
jasminpartanen <jasmin.partanen@outlook.com>
2022-02-17 16:48:28 +0200

    Plainlistitem modifications

--- components/Lists/PlainListItem.js
+++ components/Lists/PlainListItem.js
@@ -3,25 +3,35 @@ import {StyleSheet} from 'react-native';
 import PropTypes from 'prop-types';
 import {Avatar, ListItem, Text} from '@ui-kitten/components';
 import {PointRightArrow} from '../elements/Icons';
+import {uploadsUrl} from '../../utils/url';
+import moment from 'moment';
 
 // SingleItem for vertical lists
-const PlainListItem = ({navigation, singleItem}) => {
+const PlainListItem = ({navigation, singleItem, displayText}) => {
   return (
-    <ListItem style={{flex: 1, flexDirection: 'row'}}>
+    <ListItem
+      style={{flex: 1, flexDirection: 'row'}}
+      onPress={() => {
+        navigation.navigate('Product Detail', {file: singleItem});
+      }}
+    >
       <Avatar
         shape="square"
         size={'giant'}
-        source={{uri: singleItem.thumbnails.w160}}
+        source={{uri: uploadsUrl + singleItem.thumbnails.w160}}
       />
       <ListItem style={styles.ListItemDetails}>
         <Text category={'h6'} style={{fontWeight: '500', marginBottom: 5}}>
           {singleItem.title}
         </Text>
-        <Text category={'p1'}>{singleItem.price}</Text>
       </ListItem>
-      <Text category={'p2'} style={{flex: 2}}>
-        {singleItem.published}
-      </Text>
+      {displayText === true ? (
+        <Text style={{flex: 2, fontSize: 10}}>
+          {moment(singleItem.time_added).format('DD.MM.YYYY hh:mm a')}
+        </Text>
+      ) : (
+        <Text>{''}</Text>
+      )}
       <ListItem style={{flex: 1}} accessoryRight={PointRightArrow} />
     </ListItem>
   );
