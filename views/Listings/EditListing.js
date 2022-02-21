b8e839def136203bb176d051f9c3f2e4286559ab
anniehuynh <anniehuynh253@gmail.com>
2022-02-21 16:27:35 +0200

    moved AddListing.js to Lists

--- views/Listings/EditListing.js
+++ views/Listings/EditListing.js
@@ -1,7 +1,7 @@
-import React, {useContext, useState} from 'react';
+// Import from React
+import React, {useContext} from 'react';
 import PropTypes from 'prop-types';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
+import {Controller, useForm} from 'react-hook-form';
 import {
   ScrollView,
   StyleSheet,
@@ -12,9 +12,12 @@ import {
   Keyboard,
   Platform,
 } from 'react-native';
-import * as ImagePicker from 'expo-image-picker';
-import {Controller, useForm} from 'react-hook-form';
-import {Button, Card, Input, Text} from '@ui-kitten/components';
+
+// Import from UI KItten Library
+import {Card, Input} from '@ui-kitten/components';
+
+// Import from files
+import {getToken} from '../../hooks/CommonFunction';
 import {useMedia} from '../../hooks/MediaHooks';
 import {MainContext} from '../../contexts/MainContext';
 import {uploadsUrl} from '../../utils/url';
@@ -24,11 +27,8 @@ import ErrorMessage from '../../components/elements/ErrorMessage';
 
 const EditListing = ({navigation, route}) => {
   const {file} = route.params;
-  const {putMedia, loading} = useMedia();
+  const {putMedia} = useMedia();
   const {update, setUpdate} = useContext(MainContext);
-  const [image, setImage] = useState();
-  const [type, setType] = useState('image');
-  const [imageSelected, setImageSelected] = useState(false);
 
   const {
     control,
@@ -44,14 +44,14 @@ const EditListing = ({navigation, route}) => {
   // Submit Changes
   const onSubmit = async (data) => {
     try {
-      const token = await AsyncStorage.getItem('userToken');
+      const token = await getToken();
       const response = await putMedia(data, token, file.file_id);
       console.log('edit post response', response);
 
       response &&
-        Alert.alert('File', 'edited', [
+        Alert.alert('Updated', 'Post updated succesfully', [
           {
-            text: 'Ok',
+            text: 'OK',
             onPress: () => {
               setUpdate(update + 1);
               navigation.navigate('My Listings');
@@ -69,15 +69,7 @@ const EditListing = ({navigation, route}) => {
       behavior={Platform.OS === 'ios' ? 'padding' : ''}
     >
       <TouchableOpacity onPress={() => Keyboard.dismiss()} activeOpacity={1}>
-        <Card
-          style={{
-            // flex: 2,
-            width: '100%',
-            top: 130,
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-        >
+        <Card style={styles.card}>
           <ScrollView>
             <Image
               source={{uri: uploadsUrl + file.filename}}
@@ -123,7 +115,6 @@ const EditListing = ({navigation, route}) => {
               render={({field: {onChange, onBlur, value}}) => (
                 <Input
                   label="Description"
-                  // style={styles.input}
                   style={{
                     margin: 10,
                     width: 250,
@@ -144,11 +135,9 @@ const EditListing = ({navigation, route}) => {
             />
 
             <FormButton
-              // loading={loading}
-              style={styles.button}
               size="medium"
+              style={styles.button}
               title="Save"
-              // onPress={handleSubmit(onSubmit)}
               handleSubmit={handleSubmit}
               onSubmit={onSubmit}
               text="Save changes"
@@ -161,21 +150,22 @@ const EditListing = ({navigation, route}) => {
 };
 
 const styles = StyleSheet.create({
-  button: {
-    // flex: 1,
-    // justifyContent: 'center',
-    // alignItems: 'center',
-    // marginBottom: 10,
-    // marginTop: 25,
-  },
+  button: {marginTop: 20},
   container: {
     flex: 1,
     justifyContent: 'flex-start',
     alignItems: 'center',
+    backgroundColor: colors.primary,
+  },
+  card: {
+    top: '10%',
+    height: 600,
+    justifyContent: 'center',
+    alignItems: 'center',
     backgroundColor: colors.container,
   },
   image: {
-    width: '80%',
+    width: 250,
     height: undefined,
     alignSelf: 'center',
     aspectRatio: 1,
