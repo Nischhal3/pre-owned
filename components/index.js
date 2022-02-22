8b86c23de2b4c362809daf2158b33752c7e386a1
anniehuynh <anniehuynh253@gmail.com>
2022-02-22 13:55:42 +0200

    add index.js to export components

---
+++ components/index.js
@@ -0,0 +1,7 @@
+// Export components
+export {default as CategoryPicker} from './CategoryPicker';
+export {default as ExploreList} from './ExploreList';
+export {default as ImageDetail} from './ImageDetail';
+export {default as LoginForm} from './LoginForm';
+export {default as Screen} from './Screen';
+export {default as SignupForm} from './SignupForm';
