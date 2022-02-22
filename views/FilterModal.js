561061f0e99a26f2e4a23f3d11036c3e553dfc9f
jasminpartanen <jasmin.partanen@outlook.com>
2022-02-22 23:54:07 +0200

    RecentlyAdded.js deleted, created file for modal logic and view

---
+++ views/FilterModal.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Layout, Text} from '@ui-kitten/components';
+
+const FilterModal = () => {
+  return (
+    <Layout>
+      <Text>Welcome to modal</Text>
+    </Layout>
+  );
+};
+
+export default FilterModal;
