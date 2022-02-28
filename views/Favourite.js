8e4dc40d472ca0a24617d2db7f9031984cab61bb
nischhas <nischhal.shrestha@metropolia.fi>
2022-02-28 11:08:51 +0200

    user favourite list displayed

--- views/Favourite.js
+++ views/Favourite.js
@@ -1,13 +1,71 @@
-import {Text, SafeAreaView} from 'react-native';
-import React from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
+import React, {useContext, useEffect, useState} from 'react';
 import {colors} from '../utils';
+import {getMediaById, useFavourite} from '../hooks/MediaHooks';
+import {getToken} from '../hooks/CommonFunction';
+import {List} from '@ui-kitten/components';
+import {PlainListItem} from '../components/lists';
+import {ItemSeparator} from '../components/elements/ItemSeparator';
+import {MainContext} from '../contexts/MainContext';
 
-const Favourite = () => {
+const Favourite = ({navigation}) => {
+  const {getFavourtiesList} = useFavourite();
+  const [favorites, setFavourites] = useState([]);
+  const [favoriteList, setFavouriteList] = useState([]);
+  const {updateFavourite} = useContext(MainContext);
+
+  // Fetching  user favourite list
+  const list = async () => {
+    const token = await getToken();
+    const response = await getFavourtiesList(token);
+    setFavourites(response);
+  };
+
+  // Mapping and storing all the favourites file via file id on the main media list
+  const setList = async () => {
+    const media = await Promise.all(
+      favorites.map(async (favorite) => {
+        const response = await getMediaById(favorite.file_id);
+        return response;
+      })
+    );
+    setFavouriteList(media);
+  };
+
+  // Updating favourite list if user likes or dislikes the media
+  useEffect(() => {
+    list();
+  }, [updateFavourite]);
+
+  // Updating list whenever there is change in favourite
+  useEffect(() => {
+    setList();
+  }, [favorites]);
+
   return (
     <SafeAreaView style={{flex: 1, backgroundColor: colors.background}}>
-      <Text></Text>
+      <List
+        data={favoriteList}
+        contentContainerStyle={styles.container}
+        horizontal={false}
+        ItemSeparatorComponent={ItemSeparator}
+        showsHorizontalScrollIndicator={false}
+        renderItem={({item}) => (
+          <PlainListItem
+            navigation={navigation}
+            singleItem={item}
+            displayText={true}
+            showMyMedia={false}
+          />
+        )}
+      />
     </SafeAreaView>
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 20,
+  },
+});
 export default Favourite;
