6263c1705369f1a57a1425986c65c9d5ed1ef018
nischhas <nischhal.shrestha@metropolia.fi>
2022-03-07 19:51:52 +0200

    removed console log

--- views/profile/Profile.js
+++ views/profile/Profile.js
@@ -53,8 +53,6 @@ const Profile = ({route}) => {
     if (userComments.length > 0) return userComments;
   });
 
-  console.log(myMessages);
-
   // Get count for posts liked by user
   const myFavourites = mediaArray.filter((file) => {
     const userFavourites = file.fileFavourites.filter(
