173919b9aab5c3c426255fc6dba1a5336c7bbc70
Boris Hoi <Boris.Hoi@metropolia.fi>
2022-03-09 18:23:55 +0200

    Favourite landscape username position fix

--- components/lists/FavouriteList.js
+++ components/lists/FavouriteList.js
@@ -1,6 +1,6 @@
 // Import from React and library
 import React, {useEffect, useState} from 'react';
-import {Platform, StyleSheet, TouchableOpacity, View} from 'react-native';
+import {Platform, StyleSheet, TouchableOpacity, View, Dimensions} from 'react-native';
 import PropTypes from 'prop-types';
 
 // Import from UI Kitten Library
@@ -12,9 +12,17 @@ import {uploadsUrl} from '../../utils/url';
 import {LikeComponent} from '../index';
 import {getUserById} from '../../hooks/ApiHooks';
 
+// Import screen orientation
+import screenOrientation from '../../components/screenOrientation';
+
 // SingleItem for vertical lists
 const FavouriteList = ({navigation, singleItem}) => {
   const [itemUser, setItemUser] = useState({});
+
+  // Screen orientation
+  const [orientation, setOrientation] = useState(
+    screenOrientation.isPortrait() ? 'portrait' : 'landscape'
+  );
 
   const getUser = async () => {
     const user = await getUserById(singleItem.user_id);
@@ -23,45 +31,88 @@ const FavouriteList = ({navigation, singleItem}) => {
 
   useEffect(() => {
     getUser();
+    Dimensions.addEventListener('change', () => {
+      setOrientation(screenOrientation.isPortrait() ? 'portrait' : 'landscape');
+    });
   }, []);
 
-  return (
-    <View
-      style={{
-        flexd: 'row',
-        backgroundColor: colors.box,
-        borderRadius: 7,
-        borderWidth: 1,
-        borderColor: colors.lightGrey,
-        marginHorizontal: 10,
-      }}
-    >
-      <TouchableOpacity
-        style={styles.row}
-        onPress={() => {
-          navigation.navigate('Product Detail', {file: singleItem});
+  if (orientation === 'portrait') {
+    return (
+      <View
+        style={{
+          flexd: 'row',
+          backgroundColor: colors.box,
+          borderRadius: 7,
+          borderWidth: 1,
+          borderColor: colors.lightGrey,
+          marginHorizontal: 10,
         }}
       >
-        <Layout style={styles.layout}>
-          <Avatar
-            shape="square"
-            size={'giant'}
-            style={styles.productImage}
-            source={{uri: uploadsUrl + singleItem.thumbnails.w160}}
-          />
-        </Layout>
-        <Text numberOfLines={1} style={styles.title}>
-          {singleItem.title}
-        </Text>
+        <TouchableOpacity
+          style={styles.row}
+          onPress={() => {
+            navigation.navigate('Product Detail', {file: singleItem});
+          }}
+        >
+          <Layout style={styles.layout}>
+            <Avatar
+              shape="square"
+              size={'giant'}
+              style={styles.productImage}
+              source={{uri: uploadsUrl + singleItem.thumbnails.w160}}
+            />
+          </Layout>
+          <Text numberOfLines={1} style={styles.title}>
+            {singleItem.title}
+          </Text>
 
-        <Layout style={styles.user}>
-          <Text style={styles.postTitle}>Post by</Text>
-          <Text style={styles.username}>{itemUser.username}</Text>
-        </Layout>
-      </TouchableOpacity>
-      <LikeComponent file={singleItem} heartAnimation={false} />
-    </View>
-  );
+          <Layout style={styles.user}>
+            <Text style={styles.postTitle}>Post by</Text>
+            <Text style={styles.username}>{itemUser.username}</Text>
+          </Layout>
+        </TouchableOpacity>
+        <LikeComponent file={singleItem} heartAnimation={false} />
+      </View>
+    );
+  } else {
+    return (
+      <View
+        style={{
+          flexd: 'row',
+          backgroundColor: colors.box,
+          borderRadius: 7,
+          borderWidth: 1,
+          borderColor: colors.lightGrey,
+          marginHorizontal: 10,
+        }}
+      >
+        <TouchableOpacity
+          style={styles.row}
+          onPress={() => {
+            navigation.navigate('Product Detail', {file: singleItem});
+          }}
+        >
+          <Layout style={styles.layout}>
+            <Avatar
+              shape="square"
+              size={'giant'}
+              style={styles.productImage}
+              source={{uri: uploadsUrl + singleItem.thumbnails.w160}}
+            />
+          </Layout>
+          <Text numberOfLines={1} style={styles.title}>
+            {singleItem.title}
+          </Text>
+
+          <Layout style={styles.userLandscape}>
+            <Text style={styles.postTitleLandscape}>Post by</Text>
+            <Text style={styles.usernameLandscape}>{itemUser.username}</Text>
+          </Layout>
+        </TouchableOpacity>
+        <LikeComponent file={singleItem} heartAnimation={false} />
+      </View>
+    );
+  };
 };
 
 const styles = StyleSheet.create({
@@ -76,7 +127,7 @@ const styles = StyleSheet.create({
     fontSize: 14,
     top: '25%',
     fontFamily: 'Karla_400Regular',
-    left: Platform.OS === 'android' ? '-35%' : 0,
+    left: Platform.OS === 'android' ? '-38%' : 0,
     alignSelf: 'flex-end',
     color: colors.mediumGrey,
   },
@@ -106,7 +157,6 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 16,
-
     fontFamily: 'Karla_700Bold',
     alignSelf: 'center',
     width: 150,
@@ -118,6 +168,27 @@ const styles = StyleSheet.create({
     marginVertical: -10,
     top: 10,
     width: Platform.OS === 'ios' ? '85%' : '100%',
+  },
+  userLandscape: {
+    flex: 3,
+    flexDirection: 'column',
+    backgroundColor: 'transparent',
+    right: '30%',
+  },
+  postTitleLandscape: {
+    fontSize: 16,
+    top: '25%',
+    fontFamily: 'Karla_400Regular',
+    alignSelf: 'center',
+    color: colors.mediumGrey,
+  },
+  usernameLandscape: {
+    fontSize: 12,
+    fontFamily: 'Karla_400Regular',
+    alignSelf: 'center',
+    top: '30%',
+    color: colors.mediumGrey,
+    fontWeight: 'bold',
   },
 });
 
